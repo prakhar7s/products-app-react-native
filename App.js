4ce26fa1e4b4bbd401e20342eae4ed64f073d4b5
Prakhar Shrivastava <prakharshrivastava971@gmail.com>
2022-03-10 22:35:37 +0530

    changes

---
+++ App.js
@@ -0,0 +1,42 @@
+import { View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import MainNavigation from "./source/navigation/MainNavigation";
+import { CartProvider } from "./source/shop/CartProvider";
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_600SemiBold,
+  Poppins_700Bold,
+  Poppins_800ExtraBold,
+  Poppins_900Black,
+} from "@expo-google-fonts/poppins";
+import AppLoading from "expo-app-loading";
+
+export default function App() {
+  let [fontsLoaded] = useFonts({
+    POPPINS_400: Poppins_400Regular,
+    POPPINS_500: Poppins_500Medium,
+    POPPINS_600: Poppins_600SemiBold,
+    POPPINS_700: Poppins_700Bold,
+    POPPINS_800: Poppins_800ExtraBold,
+    POPPINS_900: Poppins_900Black,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return (
+    <CartProvider>
+      <View
+        style={{
+          height: 40,
+        }}
+      >
+        <StatusBar />
+      </View>
+      <MainNavigation />
+    </CartProvider>
+  );
+}
