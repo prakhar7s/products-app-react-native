209de35fadb57ccc481799fba1d13fbf9b845168
Prakhar Shrivastava <prakharshrivastava971@gmail.com>
2022-03-10 22:36:23 +0530

    constants file for images

---
+++ source/constants/images.js
@@ -0,0 +1,14 @@
+export const IMAGES = {
+  BLUE_TSHIRT: require("../assets/blue-tshirt.png"),
+  FULL_CLOTH: require("../assets/full-cloth.png"),
+  PANT: require("../assets/pant.png"),
+  RED_FULL_SLEEVE: require("../assets/red-fullsleeve.png"),
+  RED_HALF_SLEEVE: require("../assets/red-half.png"),
+  WHITE_TSHIRT: require("../assets/white-tshirt.png"),
+
+  EMPTY_CART: require("../assets/empty-cart.png"),
+
+  BANNER_ONE: require("../assets/banner-one.jpg"),
+  BANNER_TWO: require("../assets/banner-two.jpg"),
+  BANNER_THREE: require("../assets/banner-three.jpg"),
+};
