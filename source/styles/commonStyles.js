c331c5e257703b25126d28bfc2720696f22aa2b2
Prakhar Shrivastava <prakharshrivastava971@gmail.com>
2022-03-16 15:54:15 +0530

    common styles file

---
+++ source/styles/commonStyles.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+import { COLORS } from "../constants/colors";
+import { dynamicSize } from "../utils/dynamicSize";
+
+export const commonStyles = StyleSheet.create({
+  flexRow: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  flexCol: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  justifyBetween: { justifyContent: "space-between" },
+  justifyAround: { justifyContent: "space-around" },
+  padd5: {
+    padding: dynamicSize(5),
+  },
+  paddV5: {
+    paddingVertical: dynamicSize(5),
+  },
+  paddH5: {
+    paddingHorizontal: dynamicSize(5),
+  },
+  padd10: {
+    padding: dynamicSize(10),
+  },
+  paddV10: {
+    paddingVertical: dynamicSize(10),
+  },
+  paddH10: {
+    paddingHorizontal: dynamicSize(10),
+  },
+  marg5: {
+    margin: dynamicSize(5),
+  },
+  margV5: {
+    marginVertical: dynamicSize(5),
+  },
+  margH5: {
+    marginHorizontal: dynamicSize(5),
+  },
+  marg10: {
+    margin: dynamicSize(10),
+  },
+  margV10: {
+    marginVertical: dynamicSize(10),
+  },
+  margH10: {
+    marginHorizontal: dynamicSize(10),
+  },
+  borderBottom: {
+    borderBottomWidth: 1,
+    borderBottomColor: COLORS.borderColor,
+  },
+});
