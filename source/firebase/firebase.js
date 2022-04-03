e20f6a0ca705cfd420286c76dd63da0120d09832
Prakhar Shrivastava <prakharshrivastava971@gmail.com>
2022-04-03 12:10:34 +0530

    firebase setup

---
+++ source/firebase/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyAQ20jlBx53IJs6p5iErgFI_4jCx6rkNJg",
+  authDomain: "dapper-e552b.firebaseapp.com",
+  projectId: "dapper-e552b",
+  storageBucket: "dapper-e552b.appspot.com",
+  messagingSenderId: "358797068812",
+  appId: "1:358797068812:web:1a44b296cd74edaa7e7c29",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
