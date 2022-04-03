045332871b857bd06a8a774fec8a1197d635976a
Prakhar Shrivastava <prakharshrivastava971@gmail.com>
2022-04-03 12:11:12 +0530

    hiding unecesssary warnings

--- source/utils/ignoreWarningLogs.js
+++ source/utils/ignoreWarningLogs.js
@@ -6,5 +6,7 @@ export const ignoreWarningLogs = () => {
     "VirtualizedLists should never be nested",
     "Non-serializable values were found in the navigation state",
     "Clipboard has been extracted from react-native core and will be removed in a future release",
+    "Cannot update during an existing state transition (such as within `render`). Render methods should be a pure function of props and state",
+    "AsyncStorage has been extracted from react-native core and will be removed in a future release. It can now be installed and imported from '@react-native-async-storage/async-storage' instead of 'react-native'. See https://github.com/react-native-async-storage/async-storage",
   ]);
 };
